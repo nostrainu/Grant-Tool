59b1bb40af373981dc05ce42ea6a26578747ade3
y <yamivv.za@gmail.com>
2026-07-28 21:12:30 +0800

    upd

--- rejoin.py
+++ rejoin.py
@@ -11,6 +11,8 @@ import re
 import urllib.request
 import ssl
 
+os.system("clear")
+
 colors = {
     "reset": "\033[0m",
     "bold": "\033[1m",
@@ -267,7 +269,7 @@ def log_event(msg):
     recent_logs.append(f"[{t_str}] {msg}")
     draw_termux_ui()
 
-SCRIPT_VERSION = 2030
+SCRIPT_VERSION = 2031
 RAW_GITHUB_URL = "https://raw.githubusercontent.com/nostrainu/Grant-Tool/main/rejoin.py"
 
 def check_self_update():
